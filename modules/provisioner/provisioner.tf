f993ecdbe33c964db370d624c6f2331c2902ce43
Tierre Neal <tneal@hitachisolutions.com>
2023-01-06 13:20:13 -0600

    Fat Trimming

--- modules/provisioner/provisioner.tf
+++
@@ -1,6 +0,0 @@
-resource "null_resource" "PowerShellScriptRunFirstTimeOnly" {
-  provisioner "local-exec" {
-    command     = ".'${path.module}\\helpers\\Get-Processes.ps1' -First 10"
-    interpreter = ["PowerShell", "-Command"]
-  }
-}
